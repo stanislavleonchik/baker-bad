350bf3e4204d77a848d6c9cb030f8a06994d8293
Stanislav Leonchik <leonstas1@mail.ru>
2020-04-27 16:50:19 +0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(291, 51);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(179, 84);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Вес, гр.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол. шт.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(477, 171);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(219, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Открыть после сохранения?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        double weight, count;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if (textBox1.Text != "" || textBox2.Text != "")
+           {
+               try
+               {
+                   weight = double.Parse(textBox1.Text);
+                   count = double.Parse(textBox2.Text);
+               }
+               catch (FormatException)
+               {
+                   MessageBox.Show("Отсутствую или ошибочные данные!");
+                   return;
+               }
+               bread breakinBad = new bread(weight, count);
+               textBox3.Text = breakinBad.CountingProducts();
+                button2.Visible = true;
+                checkBox1.Visible = true;
+           }
+        }
+        private bool closeCheckBox = true;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Word._Application oWord;
+            Word._Document oDoc;
+            oWord = new Word.Application();
+            oWord.Visible = true;
+            oDoc = oWord.Documents.Open(Environment.CurrentDirectory + "\\blank-spravka.doc");
+            bread breakinBad = new bread(weight, count);
+            oDoc.Bookmarks["Вес"].Range.Text = Convert.ToString(breakinBad.weight);
+            oDoc.Bookmarks["Количество"].Range.Text = Convert.ToString(breakinBad.count);
+            oDoc.Bookmarks["Продукты"].Range.Text = breakinBad.CountingProducts();
+            oDoc.SaveAs2(FileName: Environment.CurrentDirectory + "\\Владимир Бланк");
+            if (closeCheckBox == true)
+            {
+                oDoc.Close();
+                oWord.Quit();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            closeCheckBox = false;
+        }
+    }
+    public class bread
+    {
+        public double weight;
+        public double count;
+        public bread(double weight, double count)
+        {
+            if ((weight > 0) || (count > 0))
+            {
+                this.weight = weight;
+                this.count = count;
+            }
+            else MessageBox.Show("Должно быть положительное число");
+        }
+        public string CountingProducts()
+        {
+            string bufer = Convert.ToString(weight * 0.55 * count) + "гр. муки\r\n";
+            bufer += Convert.ToString(weight * 0.4 * count) + "гр. воды\r\n";
+            bufer += Convert.ToString(weight * 0.05 * count) + "гр. дрожжей";
+            return bufer;
+        }
+    }
+}
